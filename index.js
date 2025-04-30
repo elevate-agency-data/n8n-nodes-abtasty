64e5f6b27953e1c35cc5779104c05630700a850d
guiElevate <94554788+guiElevate@users.noreply.github.com>
2025-04-30 09:47:57 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // This file ensures n8n can find and load your nodes and credentials
-const { ABTasty } = require('./dist/nodes/ABTasty/ABTasty.node.js');
+const { ABTasty } = require('./dist/nodes/AbTasty/AbTasty.node.js');
 
 module.exports = {
 	nodeTypes: {
 		ABTasty: ABTasty,
 	},
 	credentialTypes: {
-		ABTastyApi: require('./dist/credentials/ABTastyApi.credentials.js').ABTastyApi,
+		ABTastyApi: require('./dist/credentials/AbTastyApi.credentials.js').AbTastyApi,
 	},
 };
